d8cf92918c322a75d8d24c5366906ba81bfa8723
Tufayel Ahmed <bus.card.city@gmail.com>
2021-10-20 00:55:02 +0600

    Solved

---
+++ app/src/main/java/com/example/buscard/HistoryAdapter.java
@@ -0,0 +1,53 @@
+package com.example.buscard;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+
+public class HistoryAdapter extends FirebaseRecyclerAdapter<History,HistoryAdapter.historyviewholder>
+{
+    public HistoryAdapter(@NonNull FirebaseRecyclerOptions<History> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull historyviewholder holder, int position, @NonNull History history)
+    {
+        holder.cardNo.setText(history.getCardNo());
+        holder.date.setText(history.getDate());
+        holder.endingPoint.setText(history.getEndingPoint());
+        holder.fare.setText(history.getFare());
+        holder.startingPoint.setText(history.getStartingPoint());
+    }
+
+    @NonNull
+    @Override
+    public historyviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.historyrow,parent,false);
+        return new historyviewholder(view);
+    }
+
+    class historyviewholder extends RecyclerView.ViewHolder
+    {
+        TextView cardNo, date, distance, endingPoint, fare, startingPoint;
+        public historyviewholder(@NonNull View itemView) {
+            super(itemView);
+            cardNo = itemView.findViewById(R.id.cardNoId);
+            date = itemView.findViewById(R.id.dateId);
+            distance = itemView.findViewById(R.id.distanceId);
+            endingPoint = itemView.findViewById(R.id.endingPointId);
+            fare = itemView.findViewById(R.id.fareId);
+            startingPoint = itemView.findViewById(R.id.startingPointId);
+        }
+    }
+}
+
